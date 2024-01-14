899749851fc463decb92804c6a26b6e7ceb5e7d8
luckysetiawan <luckysetiawan0802@gmail.com>
2024-01-14 19:50:02 +0700

    add angular material and update navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterLink ],
+  imports: [ RouterLink, MatToolbarModule, MatButtonModule, MatIconModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
